44af4502dbf6529ae44b4e517cd336c4c880b24c
unknown <martinyedieke88@gmail.com>
2022-08-08 17:24:10 +0100

    my-react

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Body.css';
+function Body() {
+    return(
+        <div className='doc'>
+            <h1>Make <br />remote work</h1>
+            
+            <p>Get your team in sync. no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+            <button>Learn more</button>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+    return (
+        <div>
+            <nav className='best'>
+                <ul className='bk'>
+                    <li className='snp'>snap</li>
+                    <li>Features</li>
+                    <li>Company</li>
+                    <li>Careers</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
